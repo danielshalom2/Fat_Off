b48e2f73a760c65c99b4fcde87e53ec2f1745467
VladGesin <gasinvlad@gmail.com>
2020-05-31 20:32:47 +0300

    FinalPassReset

--- Fat_Off/src/com/FatOff/View/ResetPassWin.java
+++ Fat_Off/src/com/FatOff/View/ResetPassWin.java
@@ -19,15 +19,18 @@ import javax.swing.JOptionPane;
 import java.awt.event.ActionListener;
 import java.io.File;
 import java.awt.event.ActionEvent;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.LayoutStyle.ComponentPlacement;
 
 public class ResetPassWin {
 	private final JButton ResetEmail = new JButton("Send Validation Code");
 	private JTextField validCode;
 	private JTextField newPass;
 	private JTextField confPass;
-	private JTextField IDreset;
 	private String valCode;
 	private Object nut;
+	private JTextField IDreset;
 	
 	public ResetPassWin() {
 		JFrame resetFrame= new JFrame("PassReset");
@@ -74,15 +77,12 @@ public class ResetPassWin {
 			}
 		});
 		ResetEmail.setBounds(0, 57, 450, 29);
-		resetFrame.getContentPane().add(ResetEmail);
 		
 		JLabel lblNewLabel = new JLabel("Validation Code");
 		lblNewLabel.setBounds(16, 98, 138, 16);
-		resetFrame.getContentPane().add(lblNewLabel);
 		
 		validCode = new JTextField();
 		validCode.setBounds(158, 95, 130, 26);
-		resetFrame.getContentPane().add(validCode);
 		validCode.setColumns(10);
 		
 		JButton changeBtn = new JButton("Confirm");
@@ -100,7 +100,6 @@ public class ResetPassWin {
 		});
 		changeBtn.setEnabled(false);
 		changeBtn.setBounds(300, 161, 117, 29);
-		resetFrame.getContentPane().add(changeBtn);
 		
 		JButton checkBtn = new JButton("Check Code");
 		checkBtn.addActionListener(new ActionListener() {
@@ -118,46 +117,113 @@ public class ResetPassWin {
 			}
 		});
 		checkBtn.setBounds(300, 95, 117, 29);
-		resetFrame.getContentPane().add(checkBtn);
 		
 		JLabel text = new JLabel("New Password");
 		text.setBounds(16, 138, 104, 16);
-		resetFrame.getContentPane().add(text);
 		
 		JLabel text2 = new JLabel("Confirm Password");
 		text2.setBounds(16, 166, 114, 16);
-		resetFrame.getContentPane().add(text2);
 		
 		newPass = new JTextField();
 		newPass.setEnabled(false);
 		newPass.setEditable(false);
 		newPass.setBounds(158, 133, 130, 26);
-		resetFrame.getContentPane().add(newPass);
 		newPass.setColumns(10);
 		
 		confPass = new JTextField();
 		confPass.setEnabled(false);
 		confPass.setEditable(false);
 		confPass.setBounds(158, 161, 130, 26);
-		resetFrame.getContentPane().add(confPass);
 		confPass.setColumns(10);
 		
 		
 		JLabel lblNewLabel_1 = new JLabel("Enter ID Account");
 		lblNewLabel_1.setBounds(6, 29, 117, 16);
-		resetFrame.getContentPane().add(lblNewLabel_1);
 		
 		IDreset = new JTextField();
-		IDreset.setBounds(158, 24, 130, 26);
-		resetFrame.getContentPane().add(IDreset);
 		IDreset.setColumns(10);
+		GroupLayout groupLayout = new GroupLayout(resetFrame.getContentPane());
+		groupLayout.setHorizontalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGap(158)
+							.addComponent(confPass, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGap(158)
+							.addComponent(validCode, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGap(300)
+							.addComponent(checkBtn, GroupLayout.PREFERRED_SIZE, 117, GroupLayout.PREFERRED_SIZE))
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGap(6)
+							.addComponent(lblNewLabel_1, GroupLayout.PREFERRED_SIZE, 117, GroupLayout.PREFERRED_SIZE)
+							.addGap(34)
+							.addComponent(IDreset, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGap(158)
+							.addComponent(newPass, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGap(16)
+							.addComponent(text2))
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGap(16)
+							.addComponent(lblNewLabel, GroupLayout.PREFERRED_SIZE, 138, GroupLayout.PREFERRED_SIZE))
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGap(300)
+							.addComponent(changeBtn, GroupLayout.PREFERRED_SIZE, 117, GroupLayout.PREFERRED_SIZE))
+						.addComponent(ResetEmail, GroupLayout.PREFERRED_SIZE, 450, GroupLayout.PREFERRED_SIZE)
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGap(16)
+							.addComponent(text, GroupLayout.PREFERRED_SIZE, 104, GroupLayout.PREFERRED_SIZE)))
+					.addContainerGap(150, Short.MAX_VALUE))
+		);
+		groupLayout.setVerticalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addGap(95)
+					.addComponent(checkBtn))
+				.addGroup(groupLayout.createSequentialGroup()
+					.addGap(29)
+					.addComponent(lblNewLabel_1))
+				.addGroup(groupLayout.createSequentialGroup()
+					.addGap(166)
+					.addComponent(text2))
+				.addGroup(groupLayout.createSequentialGroup()
+					.addGap(98)
+					.addComponent(lblNewLabel))
+				.addGroup(groupLayout.createSequentialGroup()
+					.addGap(161)
+					.addComponent(changeBtn))
+				.addGroup(groupLayout.createSequentialGroup()
+					.addGap(138)
+					.addComponent(text))
+				.addGroup(groupLayout.createSequentialGroup()
+					.addGap(25)
+					.addComponent(IDreset, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGap(116)
+							.addComponent(confPass, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGap(50)
+							.addComponent(validCode, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGap(88)
+							.addComponent(newPass, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+						.addGroup(groupLayout.createSequentialGroup()
+							.addPreferredGap(ComponentPlacement.UNRELATED)
+							.addComponent(ResetEmail))))
+		);
+		resetFrame.getContentPane().setLayout(groupLayout);
 		
-<<<<<<< HEAD
+
 		resetFrame.setSize(new Dimension(600, 300));
 		
 		resetFrame.setLocationRelativeTo(null);
-=======
->>>>>>> refs/remotes/origin/master
+
 		resetFrame.setVisible(true);
 	}
 	
