379dfcb1f07190c655fecc060d7e7054e673cb36
Boris Mendelblat <boris.mand89@gmail.com>
2020-06-10 10:26:39 +0300

    Created and tested the send session summary

--- Fat_Off/src/com/FatOff/MailSender/MailSender.java
+++ Fat_Off/src/com/FatOff/MailSender/MailSender.java
@@ -3,15 +3,19 @@ package com.FatOff.MailSender;
 
 import java.io.File;
 import java.io.UnsupportedEncodingException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.*;
 
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
 import javax.mail.*;
 import javax.mail.internet.*;
 
 import com.FatOff.Controller.AdminController;
 import com.FatOff.Model.Admin;
 import com.FatOff.Model.SaveRestore;
-
 
 /**
  * This is an email sender class. It responsible for creating an email SMTP
@@ -44,7 +48,7 @@ public class MailSender {
 	 */
 
 	public MailSender(String displayName, String to) {
-		
+
 		this.from = getAdminEmailAddress();
 		this.displayName = displayName;
 		this.to = to;
@@ -82,7 +86,7 @@ public class MailSender {
 
 			message.setText("This is a reset password request for Fat_Off project!\r" + "Your validation code is: "
 					+ validationCode + "\r\r" + "Wish you all the best\rThe Fat_Off Project");
-			
+
 			Transport transport;
 			try {
 				transport = session.getTransport("smtp");
@@ -93,31 +97,72 @@ public class MailSender {
 			} catch (AuthenticationFailedException e) {
 				System.out.println("Connection unsuccessful");
 			}
-			
 
 		} catch (MessagingException | UnsupportedEncodingException mex) {
 			mex.printStackTrace();
 		}
 	}
-	
+
+	public void sendSessionSummary(String pathToFile, String custName, String nutName, String nutEmail, String nutPhone)
+			throws AddressException, MessagingException, UnsupportedEncodingException {
+
+		LocalDateTime now = LocalDateTime.now();
+
+		// Create the message object with the sender and the recipient address and set
+		// the message subject
+		Message message = new MimeMessage(this.session);
+		message.setFrom(new InternetAddress(this.from, this.displayName));
+		message.setRecipient(Message.RecipientType.TO, new InternetAddress(this.to));
+		message.setSubject("Dietician Session Summary");
+
+		// Create the message part and set the message body
+		BodyPart messageBodyPart = new MimeBodyPart();
+		messageBodyPart.setText("Dear " + custName + ",\r\r"
+				+ "After our plesent meeting and after we talked and decided on our goal, "
+				+ "now we arrived to the hard  but very important part.\r"
+				+ "In the attached file, you will find our session summary, "
+				+ "your current measures and the suggested menu until the next session.\r\r"
+				+ "For any farther questions you have or if you need any advise, "
+				+ "please feel free to contact me and I will happily assist you if I can.\r\rLooking forward to our next session.\r\r\r"
+				+ "Best Regards,\r" + nutName + "\rEmail Address: " + nutEmail + "\rPhone Number: " + nutPhone);
+
+		// Create multipart message with the message and the attachment
+		Multipart multipart = new MimeMultipart();
+		multipart.addBodyPart(messageBodyPart);
+
+		// Adding the attachments
+		messageBodyPart = new MimeBodyPart();
+		DataSource source = new FileDataSource(pathToFile);
+		messageBodyPart.setDataHandler(new DataHandler(source));
+		messageBodyPart.setFileName("Session_" + DateTimeFormatter.ofPattern("dd/MM/YYYY").format(now).toString()+".pdf");
+		multipart.addBodyPart(messageBodyPart);
+
+		// Send the complete message parts
+		message.setContent(multipart);
+
+		// Send the message
+		Transport.send(message);
+
+	}
+
 	private String getAdminAppPass() {
 		String adminName = "";
 		File adminToRestore = new File(SaveRestore.getPath() + "/Admin");
-		String [] desired = adminToRestore.list();
-		for(int i = 0 ; i < desired.length ; i++) {
-			if(!desired[i].equals(".DS_Store"))
+		String[] desired = adminToRestore.list();
+		for (int i = 0; i < desired.length; i++) {
+			if (!desired[i].equals(".DS_Store"))
 				adminName = desired[i];
 		}
 		Admin adm = AdminController.restoreAdmin(adminName);
 		return adm.getEmailAppPass();
 	}
-	
+
 	private String getAdminEmailAddress() {
 		String adminName = "";
 		File adminToRestore = new File(SaveRestore.getPath() + "/Admin");
-		String [] desired = adminToRestore.list();
-		for(int i = 0 ; i < desired.length ; i++) {
-			if(!desired[i].equals(".DS_Store"))
+		String[] desired = adminToRestore.list();
+		for (int i = 0; i < desired.length; i++) {
+			if (!desired[i].equals(".DS_Store"))
 				adminName = desired[i];
 		}
 		Admin adm = AdminController.restoreAdmin(adminName);
